e395afc60355a7f2558898087f7d2b2d420139dc
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-09-30 20:50:08 -0700

    done

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve("hi");
-    } else {
-      reject("bye");
-    }
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
