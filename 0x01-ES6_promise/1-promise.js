4b05389596101e71b9200f57dc791dd8de0d6d0d
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-09-30 21:07:42 -0700

    done

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,9 +3,10 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({
         status: 200,
-	body: 'Success',
-      })} else {
-          reject(new Error("The fake API is not working currently"));
-      }
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
   });
 }
