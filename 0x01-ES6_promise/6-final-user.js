8c1387c81df36a85fd2f74b7657f72930d9db1b9
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-10-01 00:12:27 -0700

    done

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise
     .allSettled(promises)
-    .then((Results) => Results.map((Result) => ({ status: Result.status, value: Result.value })));
+    .then((Results) => Results.map((Result) => ({ status: Result.status, value: Result.result })));
 }
