b1afa064befe5dee6e042e85661d6c240c220ccc
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-10-05 02:15:20 -0700

    done

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent + curStudent.id, 0,
+    );
+  }
+  return 0;
+}
