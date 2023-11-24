15cb732415eec5d604d617fbe00807adb7413af3
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-11-24 00:34:23 +0000

    done

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API integration test', () => {
+
+  it('GET / returns correct response', (done) => {
+    request.get(`http://localhost:7865/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
